be4e2984ce169b7021248303d16790f67ad4367f
Toan <nguyenquoctoan1230@gmail.com>
2023-03-14 11:08:03 +0700

    add assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const logoDark = require('./images/logo-dark.png');
+const appIcon = require('./images/favicon.png');
+
+const assets = {
+	images: { logoDark, appIcon },
+	colors: {
+		secondary: '#292929',
+	},
+};
+
+export default assets;
