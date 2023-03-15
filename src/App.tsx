57b859a375ab60d007084ec7958681be8a2f079d
Toan <nguyenquoctoan1230@gmail.com>
2023-03-15 12:57:55 +0700

    setup theme and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,54 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import AuthLayout from './layout/AuthLayout';
+import AppLayout from './layout/AppLayout';
+
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+	{
+		element: <AppLayout />,
+		errorElement: <NotFound />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+		],
+	},
+	{
+		element: <AuthLayout />,
+		children: [
+			{
+				path: '/signup',
+				element: <Signup />,
+			},
+			{
+				path: '/login',
+				element: <Login />,
+			},
+		],
+	},
+]);
 
 function App(): JSX.Element {
+	const theme = createTheme({
+		palette: { mode: 'dark' },
+	});
+
 	return (
-		<div>
-			Kanban app
-		</div>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	);
 }
 
