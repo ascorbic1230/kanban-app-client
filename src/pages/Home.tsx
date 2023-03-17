8b26ac66616c40667bc759f87e2021cf3356f9ed
Toan <nguyenquoctoan1230@gmail.com>
2023-03-17 16:38:54 +0700

    Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+import { Box } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+
+import boardApi from '../api/boardApi';
+import { setCurrentBoard } from '../redux/features/boardSlice';
 
 function Home(): JSX.Element {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { enqueueSnackbar } = useSnackbar();
+
+	const [loading, setLoading] = useState(false);
+
+	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+	const createBoard = async () => {
+		setLoading(true);
+		try {
+			const res = await boardApi.createBoard();
+			dispatch(setCurrentBoard(res.data.board));
+			navigate(`/boards/${res.data.board.id}`);
+		} catch (error: any) {
+			enqueueSnackbar(error.message, { variant: 'error' });
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
-		<div>Home</div>
+		<Box
+			sx={{
+				height: '100%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<LoadingButton
+				variant="outlined"
+				color="success"
+				onClick={() => { createBoard(); }}
+				loading={loading}
+			>
+				Click here to create your first board
+			</LoadingButton>
+		</Box>
 	);
 }
 
