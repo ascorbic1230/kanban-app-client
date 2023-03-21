a5e139cb8e39dbfe69e7e4d5b035e02b9abe5f1b
Toan <nguyenquoctoan1230@gmail.com>
2023-03-21 14:54:01 +0700

    custom style scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './styles/custom-scrollbar.css';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { SnackbarProvider } from 'notistack';
